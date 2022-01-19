0e8fff8707afa54233a6bec3b2734db92e8150cb
Kuro2000 <ducthang200023@gmail.com>
2022-01-19 16:10:32 +0700

    removed template made by create-react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import './App.css'
+import Homepage from './pages/Homepage'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Learn React
-                </a>
-            </header>
+            <Header/>
+            <Homepage/>
+            <Footer/>
         </div>
     )
 }
