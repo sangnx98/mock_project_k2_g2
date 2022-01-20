edd506c64c6e57b4580c6816af5c258ac0678958
ngoclamm <ngoclamm0612@gmail.com>
2022-01-20 11:22:26 +0700

    feature/landing/lamngoc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Homepage from './pages/Homepage'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Landing from './pages/Landingpage'
 
 function App() {
     return (
         <div className='App'>
+            <Landing />
             <Header/>
             <Homepage/>
             <Footer/>
