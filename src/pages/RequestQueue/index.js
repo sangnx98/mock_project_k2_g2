b902d354d5421f16b8e8f68f1a8d51ae67ed83b7
Nguyen Xuan Sang <sangnx1998@gmail.com>
2022-01-20 22:10:05 +0700

    create tournament page with match history + participant

---
+++ src/pages/RequestQueue/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import './index.css'
+
+export default function PlayerList() {
+    return (
+        <div className='tournament-participant-list'>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+            <div className='tournament-participant-list-item'>
+                <p>Player 1</p>
+                <div className='list-item-button'>
+                    <button className='list-item-button-common'>Approve</button>
+                    <button className='list-item-button-common'>Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
