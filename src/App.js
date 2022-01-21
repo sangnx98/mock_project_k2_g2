6673d0126fe347b8996ec3997be6a2ec50140170
Duongduchuynh <duchuynh4799@gmail.com>
2022-01-21 14:56:06 +0700

    display static Edit Tournament

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import MatchDetail from './pages/MatchDetail'
+import TournamentEdit from './pages/TournamentEdit'
 import Footer from './components/Footer'
-// import Header from './components/Header'
+import Header from './components/Header'
 
 
 function App() {
     return (
         <div className='App'>
-            <MatchDetail/>
-            <Footer/>
-            {/* <Header/> */}
+            <Header />
+            <TournamentEdit />
+            <Footer />
+
         </div>
     )
 }
