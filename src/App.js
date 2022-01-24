364f2f2326a4069540caf20372eb0e6c3816d37d
Kuro2000 <ducthang200023@gmail.com>
2022-01-25 00:35:46 +0700

    Added context file and tournament list context

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import AppContextProvider from './contexts/globalContext'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import TournamentList from './pages/TournamentList'
 import TournamentPage from './pages/TournamentPage'
 import Landingpage from './pages/Landingpage'
 import UserRegister from './pages/UserRegister'
 import UserLogin from './pages/UserLogin'
 import TournamentCreate from './pages/TournamentCreate'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
     const [isLoggedIn] = React.useState(false)
 
     return (
         <div className='App'>
-            <Header loginState={isLoggedIn}/>
+            <AppContextProvider>
+                <Header loginState={isLoggedIn}/>
 
-            <Routes>
-                <Route exact path='/' element={<Landingpage/>}/>
-                <Route path='/user/register' element={<UserRegister/>}/>
-                <Route path='/user/login' element={<UserLogin/>}/>
-                <Route path='/tournaments' element={<TournamentList/>}/>
-                <Route path={'/tournaments/:id'} element={<TournamentPage/>}/>
-                <Route path='/tournaments/create' element={<TournamentCreate/>}/>
-            </Routes>
+                <Routes>
+                    <Route exact path='/' element={<Landingpage/>}/>
+                    <Route path='/user/register' element={<UserRegister/>}/>
+                    <Route path='/user/login' element={<UserLogin/>}/>
+                    <Route path='/tournaments' element={<TournamentList/>}/>
+                    <Route path={'/tournaments/:id'} element={<TournamentPage/>}/>
+                    <Route path='/tournaments/create' element={<TournamentCreate/>}/>
+                </Routes>
 
-            <Footer/>
+                <Footer/>
+            </AppContextProvider>
+            
         </div>
     )
 }
