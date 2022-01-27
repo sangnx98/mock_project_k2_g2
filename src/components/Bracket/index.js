be6a5e758bc6cf49215cd3cbc8f818e22573f9a7
Kuro2000 <ducthang200023@gmail.com>
2022-01-27 09:17:31 +0700

    Added Bracket Validation

--- src/components/Bracket/index.js
+++ src/components/Bracket/index.js
@@ -1,13 +1,46 @@
 /* eslint-disable no-unused-vars */
 import React, {useContext, useEffect} from 'react'
 import './index.css'
+import { AppContext } from '../../contexts/globalContext'
+import db from '../../configs/firebaseConfig'
 
-export default function Bracket() {    
+const sortByScore = (arrObj) => {
+    
+}
 
-    return (
-        <>
-            <h1 className='playerRequiredText'>You should have 2 or more participant to view bracket</h1>
-        </>
-    )
+export default function Bracket({tournamentId, format}) {    
+    const {participants, getParticipants} = useContext(AppContext)
+
+    useEffect(() => {
+        getParticipants()
+    }, [])
+
+    let participantsData = []
+    let matches = []
+
+    sortByScore(participantsData)
+
+    Object.keys(participants).map(itemId =>{
+        if(participants[itemId].tournamentId == tournamentId){
+            participantsData.push(participants[itemId])
+        }
+
+    })
+
+    console.log(participantsData)
+
+    if(participantsData.length>32) return(<p className='playerRequiredText'>Tournament currently not support more than 32 participants</p>)
+
+    if(participantsData.length<2) return(<p className='playerRequiredText'>You should have 2 or more participant to view bracket</p>)
+
+    if(format == 'Leaderboard') return(<div>
+        <p>This is Leaderboard bracket</p>
+    </div>)
+
+    if(format == 'Single Elimination') return(<div>
+        <p>This is Single Elimination bracket</p>
+    </div>)
+
+    else return(<p>Setting up, please wait or contact the host for more information</p>)
 
 }
